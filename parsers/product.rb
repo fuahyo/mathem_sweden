df9ed403043f1ef11426cf89b5cd5c4714bfc576
bunxbio <biokenfigumilang@gmail.com>
2022-07-22 19:19:50 +0700

    enabling product parser

---
+++ parsers/product.rb
@@ -0,0 +1,22 @@
+info = page["vars"]["info"]
+html = Nokogiri.HTML(content)
+
+info["_collection"] = "product"
+info["_id"] = info["competitor_product_id"]
+info["scraped_at_timestamp"] = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+info["description"] = html.at_css("mathem-product-details-accordion p").text.strip rescue nil
+
+info["is_promoted"] = false
+info["type_of_promotion"] = nil
+info["promo_attributes"] = nil
+
+promo = html.at_css(".product .product-overlay .splash-container").text.strip rescue nil
+if promo
+    info["is_promoted"] = true
+    info["type_of_promotion"] = "badge"
+    info["promo_attributes"] = JSON.generate({
+        "promo_detail" => promo,
+    })
+end
+
+outputs << info
