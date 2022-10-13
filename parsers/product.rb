e579bb3b6f2b5b3daddd4ef06ba837a2fff23b0d
bunxbio <biokenfigumilang@gmail.com>
2022-10-13 22:33:17 +0700

    add exception

--- parsers/product.rb
+++ parsers/product.rb
@@ -1,25 +1,29 @@
 info = page["vars"]["info"]
 html = Nokogiri.HTML(content)
 
-info["_collection"] = "products"
-info["_id"] = info["competitor_product_id"]
-info["scraped_at_timestamp"] = Time.now.strftime('%Y-%m-%d %H:%M:%S')
-#info["description"] = html.at_css("mathem-product-details-accordion p").text.strip rescue nil
-info["description"] = html.at_css("mathem-product-details-accordion").text.strip rescue nil
+if content.nil? || html.at_css("h1").nil?
+    raise "failed fetch page, please refetch"
+else
+    info["_collection"] = "products"
+    info["_id"] = info["competitor_product_id"]
+    info["scraped_at_timestamp"] = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    #info["description"] = html.at_css("mathem-product-details-accordion p").text.strip rescue nil
+    info["description"] = html.at_css("mathem-product-details-accordion").text.strip rescue nil
 
-info["is_promoted"] = false
-info["type_of_promotion"] = nil
-info["promo_attributes"] = nil
+    info["is_promoted"] = false
+    info["type_of_promotion"] = nil
+    info["promo_attributes"] = nil
 
-promo = html.at_css(".product .product-overlay .splash-container").text.strip rescue nil
-if promo
-    unless promo =~ /Snart\s?i\s?lager/i
-        info["is_promoted"] = true
-        info["type_of_promotion"] = "badge"
-        info["promo_attributes"] = JSON.generate({
-            "promo_detail" => "'#{promo}'",
-        })
+    promo = html.at_css(".product .product-overlay .splash-container").text.strip rescue nil
+    if promo
+        unless promo =~ /Snart\s?i\s?lager/i
+            info["is_promoted"] = true
+            info["type_of_promotion"] = "badge"
+            info["promo_attributes"] = JSON.generate({
+                "promo_detail" => "'#{promo}'",
+            })
+        end
     end
-end
 
-outputs << info
+    outputs << info
+end
