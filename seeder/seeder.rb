dfe1a75b13e9044d6736b32d2bdcd3825f8d7340
bunxbio <biokenfigumilang@gmail.com>
2024-03-22 17:40:17 +0700

    start rebuild

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,3 +1,4 @@
+=begin
 #require './lib/headers'
 
 #store_id = 10
@@ -10,4 +11,11 @@ pages << {
 	vars: {
 		store_id: store_id,
 	}
+}
+=end
+
+pages << {
+	page_type: "categories",
+	url: "https://www.mathem.se/_next/data/dea2c15da15af05b352b732c61bdef4559fb0118/se/products.json?site=se",
+	#headers: ReqHeaders::HEADERS,
 }
