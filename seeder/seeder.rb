c84b3fd8159248fb9976058216fb2f7acedcec5f
bunxbio <biokenfigumilang@gmail.com>
2024-05-08 20:35:20 +0700

    update

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,6 +1,6 @@
 #require './lib/headers'
 
-idk = "8bc45263c419bc1b5c428745f8166b4800813e3d" #don't know what this is, but always need update
+idk = "f0cd8e68bd08433f1bad23a56af8c38f63c8ceb0" #don't know what this is, but always need update
 
 pages << {
 	page_type: "categories",
